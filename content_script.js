11ceb6e346a33517ee30422f54ba5fe2e201d42b
amirmazz <amirmazz@umich.edu>
2021-06-22 07:06:40 -0400

    Add option to use VP9 instead of H.264

--- content_script.js
+++ content_script.js
@@ -6,13 +6,15 @@ urls = [
 ]
 
 // very messy workaround for accessing chrome storage outside of background / content scripts
-chrome.storage.sync.get(['use6Channels', 'setMaxBitrate'], function(items) {
+chrome.storage.sync.get(['use6Channels', 'setMaxBitrate', 'useVP9'], function(items) {
     var use6Channels = items.use6Channels;
     var setMaxBitrate = items.setMaxBitrate;
+    var useVP9 = items.useVP9;
     var mainScript = document.createElement('script');
     mainScript.type = 'application/javascript';
     mainScript.text = 'var use6Channels = ' + use6Channels + ';' + '\n' 
-	                + 'var setMaxBitrate = ' + setMaxBitrate + ';';
+	                + 'var setMaxBitrate = ' + setMaxBitrate + ';' + '\n'
+                    + 'var useVP9 = ' + useVP9 + ';';
     document.documentElement.appendChild(mainScript);
 });
 
--- pages/options.js
+++ pages/options.js
@@ -1,9 +1,11 @@
 function save_options() {
     var use6Channels = document.getElementById('5.1').checked;
     var setMaxBitrate = document.getElementById('setMaxBitrate').checked;
+    var useVP9 = document.getElementById('useVP9').checked;
     chrome.storage.sync.set({
         use6Channels: use6Channels,
-        setMaxBitrate: setMaxBitrate
+        setMaxBitrate: setMaxBitrate,
+        useVP9: useVP9
     }, function() {
         var status = document.getElementById('status');
         status.textContent = 'Options saved.';
@@ -16,10 +18,12 @@ function save_options() {
 function restore_options() {
     chrome.storage.sync.get({
         use6Channels: false,
-        setMaxBitrate: false
+        setMaxBitrate: false,
+        useVP9: false
     }, function(items) {
         document.getElementById('5.1').checked = items.use6Channels;
         document.getElementById('setMaxBitrate').checked = items.setMaxBitrate;
+        document.getElementById('useVP9').checked = items.useVP9;
     });
 }
 
